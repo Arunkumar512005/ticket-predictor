b8c8eefb6168cf78d398bd46aec718208aeb27df
Arunkumar512005 <arunkumar47954@gmail.com>
2025-07-19 20:50:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
 
-DATABASE_URL = "postgresql://postgres:14504@localhost/ticketdb"
+DATABASE_URL = "postgresql://yourdbname@localhost/ticketdb"
 JWT_SECRET_KEY = "your-very-secret-key"  # used for JWT encoding
